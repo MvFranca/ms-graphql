fd4eada2605c8f15963a029d425d0fe08d2aeb93
mvfsilva.develcode <viniciusfranca09@hotmail.com>
2025-04-01 10:44:05 -0300

    Removendo dist e yarn.lock do controle de versão

--- dist/graphql/resolvers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-const onboardingQuiz_resolver_1 = require("./onboardingQuiz.resolver");
-const user_resolver_1 = require("./user.resolver");
-exports.resolvers = [user_resolver_1.userResolvers, onboardingQuiz_resolver_1.onboardingQuizResolver];
